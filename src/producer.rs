976d86ae2a41f3d2ac73b8567a9b29e7827661b3
Nuttiiya Seekhao <nuttiiya.seekhao@gmail.com>
2022-11-23 09:00:25 -0800

    Add BETWEEN producer

--- src/producer.rs
+++ src/producer.rs
@@ -347,9 +347,54 @@ fn _register_function(function_name: String, extension_info: &mut (Vec<extension
 
 }
 
+pub fn make_binary_op_scalar_func(lhs: &Expression, rhs: &Expression, op: Operator, extension_info: &mut (Vec<extensions::SimpleExtensionDeclaration>, HashMap<String, u32>)) -> Expression {
+    let function_name = operator_to_name(op).to_string().to_lowercase();
+    let function_anchor = _register_function(function_name, extension_info);
+    Expression {
+        rex_type: Some(RexType::ScalarFunction(ScalarFunction {
+            function_reference: function_anchor,
+            arguments: vec![
+                FunctionArgument {
+                    arg_type: Some(ArgType::Value(lhs.clone())),
+                },
+                FunctionArgument {
+                    arg_type: Some(ArgType::Value(rhs.clone())),
+                },
+            ],
+            output_type: None,
+            args: vec![],
+        })),
+    }
+}
+
 /// Convert DataFusion Expr to Substrait Rex
 pub fn to_substrait_rex(expr: &Expr, schema: &DFSchemaRef, extension_info: &mut (Vec<extensions::SimpleExtensionDeclaration>, HashMap<String, u32>)) -> Result<Expression> {
     match expr {
+        Expr::Between { expr, negated, low, high } => {
+            let l_expr;
+            let r_expr;
+            match negated {
+                true => {
+                    // `expr NOT BETWEEN low AND high` can be translated into (expr < low AND high < expr)
+                    let substriat_expr = to_substrait_rex(expr, schema, extension_info)?;
+                    let substrait_low = to_substrait_rex(low, schema, extension_info)?;
+                    let substrait_high = to_substrait_rex(high, schema, extension_info)?;
+
+                    l_expr = make_binary_op_scalar_func(&substriat_expr, &substrait_low, Operator::Lt, extension_info);
+                    r_expr = make_binary_op_scalar_func(&substrait_high, &substriat_expr, Operator::Lt, extension_info);
+                }
+                false => {
+                    // `expr BETWEEN low AND high` can be translated into (low <= expr AND expr <= high)
+                    let substriat_expr = to_substrait_rex(expr, schema, extension_info)?;
+                    let substrait_low = to_substrait_rex(low, schema, extension_info)?;
+                    let substrait_high = to_substrait_rex(high, schema, extension_info)?;
+
+                    l_expr = make_binary_op_scalar_func(&substrait_low, &substriat_expr, Operator::LtEq, extension_info);
+                    r_expr = make_binary_op_scalar_func(&substriat_expr, &substrait_high, Operator::LtEq, extension_info);
+                }
+            }
+            Ok(make_binary_op_scalar_func(&l_expr, &r_expr, Operator::And, extension_info))
+        }
         Expr::Column(col) => {
             let index = schema.index_of_column(&col)?;
             substrait_field_ref(index)
